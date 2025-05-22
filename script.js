b54f3888c255a8c3ea2abfbf4006da522a655dc9
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-05-22 09:34:42 +0530

    variables in javascript

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// var
+
+var abc = "Hello World";    //  var keyword variable
+
+let marks = 500;       // let keyword variable
+
+const his = "Pruthvij"   // const keyword variable
+
+
+const a = 500;
+
+{
+    const a = 1000;  
+
+    console.log(a);
+    
+}
+
+console.log(a);
+
+
+
+
+// The var keyword was used in all JavaScript code from 1995 to 2015.
+
+// The let and const keywords were added to JavaScript in 2015.
+
+// The var keyword should only be used in code written for older browsers.
+
+// JavaScript identifiers are case-sensitive.
