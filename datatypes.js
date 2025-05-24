8e704e791b9b27a925e54aa2d44198490b6b8584
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-05-24 11:02:15 +0530

    Javascript Datatypes

---
+++ datatypes.js
@@ -0,0 +1,98 @@
+
+//   Data Types
+
+// Primitive Data Type
+
+// JavaScript has 7 primitive data types
+
+
+// 1) String: Represents textual data, e.g., "hello"
+
+//   var firstName = "Hrishikesh";
+
+//   console.log("First Name of User :",firstName);
+
+//   console.log("The Type of above value is :",typeof firstName);
+
+
+// 2) Number: Represents numeric values, e.g., 10, 3.14.
+
+// var num = 55;
+
+// var totalValue = 9.55;
+
+// console.log("The Total Value of Sum : ",totalValue);
+
+
+// 3) Boolean: Represents logical values, true or false.
+
+// var isWin = false;
+// var isPass = true;
+// var isAlive = true;
+// var isActive = true;
+
+// console.log(typeof isWin);
+
+
+// The null data type in JavaScript represents the intentional absence of a value.It signifies that a variable or object property 
+// has no value assigned to it or that a previously assigned value has been intentionally cleared.It is a primitive value, but when 
+// using the typeof operator, it incorrectly returns "object," a known quirk in JavaScript.
+
+
+// var nullValue = null;
+// console.log(typeof nullValue);
+
+
+
+// Undefined
+// In JavaScript, a variable without a value, has the value undefined. The type is also undefined.
+
+// var emptyValue;
+
+// console.log(emptyValue);
+// console.log(typeof emptyValue);
+
+// JavaScript BigInt
+// All JavaScript numbers are stored in a 64-bit floating-point format.
+// JavaScript BigInt is a new datatype (ES2020) that can be used to store 
+// integer values that are too big to be represented by a normal JavaScript Number.
+
+// const bigIntValue1 = 1234567890123456789012345n;
+// const bigIntValue2 = BigInt("9876543210987654321098765");
+// var exampleBig = BigInt("354857468574654545465456454654564")
+
+
+// console.log(typeof bigIntValue1);
+// console.log(typeof bigIntValue2);
+// console.log(typeof exampleBig);
+
+
+// In JavaScript, the symbol data type is a primitive type introduced in ES6 (ECMAScript 2015).
+
+
+// var sym = Symbol("hello");
+// var sym2 = Symbol("hello")
+
+// console.log( sym);
+// console.log( sym2);
+
+// var compare = sym == sym2;
+
+// console.log(compare);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
