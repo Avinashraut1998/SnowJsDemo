656ca642640b94a08f2c1e5a950047aaa257b01d
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-05-26 09:52:09 +0530

    Javascript Operators

---
+++ operators.js
@@ -0,0 +1,171 @@
+// Javascript operators are used to perform different types of mathematical and logical computations.
+// JavaScript Operators
+
+
+// JavaScript Assignment
+// The Assignment Operator (=) assigns a value to a variable
+
+// let x = 100;
+
+
+
+// JavaScript Arithmetic Operators
+// Arithmetic operators perform arithmetic on numbers
+
+
+// Operator	Description
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Remainder)
+
+
+// Addition
+
+// var a = 50;
+// var b = 50;
+
+// var sum = a + b;
+// console.log(sum);
+
+
+// -	Subtraction
+
+// var c = 100;
+// var d = 25;
+
+// var ans = c - d;
+// console.log(ans);
+
+
+// *	Multiplication
+
+// let a = 5;
+// let b = 100;
+
+// let ans = a * b;
+// console.log(ans);
+
+
+// **	Exponentiation (ES2016)
+
+// let a  = 10;
+// let b = 5;
+
+// let ans = a ** b;
+// console.log(ans);
+
+
+// /	Division
+
+// let a = 10;
+// let b = 5;
+
+// let ans = a / b;
+// console.log(ans);
+
+
+// %	Modulus (Remainder)
+
+// let a = 500;
+// let b = 7;
+
+// let ans = a % b;
+// console.log(ans);
+
+
+
+// JavaScript Assignment Operators
+// Assignment operators assign values to JavaScript variables.
+
+
+/*
+=	x = y	x = y
++=	x += y	x = x + y
+-=	x -= y	x = x - y
+*=	x *= y	x = x * y
+/=	x /= y	x = x / y
+%=	x %= y	x = x % y
+**=	x **= y	x = x ** y
+*/
+
+// let a = 5;
+// let b = 2;
+
+// console.log(a = b);
+
+// let result = a += b;   // a = a + b 
+// console.log(result);
+
+
+// let result = a -= b;  // a = a - b 
+// console.log(result);
+
+// let result = a *= b;  // a = a * b
+// console.log(result);
+
+
+// let result = a /= b;  // a = a / b
+// console.log(result);
+
+// let result = a %= b;  // a = a % b
+// console.log(result);
+
+// let result = a **= b 
+// console.log(result);
+
+ 
+
+
+
+// ++	Increment
+// --	Decrement
+
+
+// let a = 500;
+// console.log(a);
+
+
+// a++;    // a = a + 1
+
+// console.log(a);
+
+//post Incriment
+// let a = 5;     
+// a++;    // a = a + 1
+// console.log(a);
+
+//pre-Incriment
+
+// let a = 5;
+// console.log(++a);
+
+
+//a--    // a = a - 1
+// var a = 5;
+// var a = a - 1   // a--
+// console.log(a);
+
+
+// let a = 5000;
+// console.log(a);
+
+// a--;              // a = a - 1
+// console.log(a);
+
+// let a = 500;
+
+// console.log(--a);
+
+
+
+
+
+
+
+
+
+
+
