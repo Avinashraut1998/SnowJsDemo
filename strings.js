2b96c6085c4f5fb6c2ae1eb48b257a7d4a207fd3
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-05-28 09:35:51 +0530

    Strings

---
+++ strings.js
@@ -0,0 +1,104 @@
+// String 
+
+// let firstName = "Vijay"    // Strings are for storing text    
+
+//Strings are written with quotes
+//  let str = 'hello'
+
+//  Quotes Inside Quotes
+//  let str2 = "This is 'basic' Javascript"
+
+//  console.log(firstName);
+//  console.log(str2);
+
+
+
+//  let strObj = new String("This is 'basic' Javascript")
+//  console.log(typeof strObj);
+ 
+
+//  let result = str2 === strObj;
+//  console.log(result);
+
+//Javascript strings are primitive and immutable: All string methods produce a new string without altering the original string.
+// JavaScript String Methods
+
+
+//length
+/*
+let Fname = "Hrishikesh"
+let ans = Fname.length
+console.log(ans);
+*/
+ 
+//toUpperCase 
+/*1
+let Fname = "hrishikesh"
+let result = Fname.toUpperCase()
+console.log(result);
+*/
+ 
+// toLowerCase
+/*
+let fName = "HRISHIKESH"
+let result = fName.toLowerCase()
+console.log(result);
+*/
+
+
+//indexOf
+/*
+let a = "I Love Coding"
+let result = a.indexOf("Coding")
+let result2 = a.indexOf("L")
+console.log(result);
+*/
+
+//Slice()
+/*
+let str = "I Love Coding"
+// let result2 = str.indexOf("C")
+let result = str.slice(7)
+console.log(result);
+*/
+
+//replace
+/*
+let a = "I Love Coding"
+let result = a.replace("Love", "Like")
+console.log(result);
+*/
+
+//repeat
+/*
+let a = "I Love Coding"
+let result = a.repeat(5)
+console.log(result);
+*/
+
+
+// Concatination
+
+// let str = "This is Our value";
+// let val = 50;
+// let str2 ="for operation";
+
+// let result = (str + " " + val + " " +str2)
+// console.log(result);
+
+// console.log("is our value:" + " " + 5   );
+
+
+
+
+// String Litrals    ---- String interpolation
+// let a = 5;
+// let b = 10;
+// let userVal = `This is string from string litrals ${a * b}, and this is our value ${b}, 
+// and the value of a is ${a * 100}
+// .
+// .
+// .
+// .
+// ${b * 5}`
+// console.log(userVal);
