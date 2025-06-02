6e6c7d64239900dcb3a7e205e5e8fdfe5f27d632
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-06-02 09:07:54 +0530

    Array

---
+++ array.js
@@ -0,0 +1,155 @@
+// 
+
+
+// let a = "Mango";
+// let b = "Apple";
+// let c = "Banana";
+// let d = "Pienapple";
+// let e = "Orange"
+
+// let fruits = ["Mango", "Apple", "Banana", "Pienapple", "Orange"];
+
+
+
+// console.log(fruits);
+
+
+// let emptyArray = [];
+
+// emptyArray[0] = "Nagpur";
+// emptyArray[1] = "Pune";
+// emptyArray[2] = "Mumbai";
+// emptyArray[3] = "Nasik";
+
+
+// console.log(emptyArray);
+
+
+// const newArr = new Array("One", "Two");
+
+// newArr[2] = "Three"
+
+// console.log(newArr);
+
+
+
+// let arr = ["One", 'Two']
+
+
+// console.log(typeof arr);
+
+
+
+// let arr = ["text", 12, true]
+
+// console.log(typeof arr);
+
+
+// let nums = [1,4,5,6,3,7,8,9,2]
+
+// console.log(nums.sort());
+
+
+// let fruits = ["Mango", "Apple", "Banana", "Pienapple", "Orange"];
+
+// // fruits.pop()
+
+
+// // fruits.push("strawberry", "Grapes")
+
+// console.log(fruits);
+
+
+
+// let fruits = ["Mango", "Apple", "Banana", "Pienapple", "Orange"];
+// console.log(fruits.join(" "));
+
+// console.log(fruits.at(2));
+// console.log(fruits.toString());
+
+
+// let arr1 = [1,2,3,4,5]
+
+// let arr2 = [6,7,8,9,10]
+
+// let arr3 = [11,12,13,14,15]
+
+
+// let result3 = arr1 + "," + arr2
+// console.log(result3);
+
+//Spread operator
+ 
+// let result1 = [...arr1, ...arr2, ...arr3]
+
+// console.log(result1);
+
+
+
+
+
+// let arr12 = [1,2,3,4,5]
+
+// let arr21 = [6,7,8,9,10]
+
+// let a3  = ["One , Two"]
+
+// let result = arr12.concat(arr21, a3)
+// console.log(result);
+
+
+
+
+// let nums = [1,2,3, [4,5,6], 7,8,9, [10,11,12], 13, 14, [15,16, [17,18,[19,20,[21,22]]]]]
+
+
+// console.log(nums.flat(4));
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.unshift("Kiwi")
+// console.log(fruits);
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.shift()
+// console.log(fruits);
+
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.reverse());
+// console.log(fruits.sort());
+
+
+
+
+// let myDate = new Date
+// console.log(myDate);
+
+
+// let val = Math
+// console.log(val);
+
+
+// let myValue = Math.random();
+// console.log(myValue);
+
+
+// let myvalue = Math.ceil(Math.random() * 10)
+
+
+// let myValue = Math.random() * 10;
+// console.log(myValue);
+
+// console.log(Math.floor(myValue));
+// console.log(Math.ceil(myValue));
+
+
+
+
+
+
+
+ 
+
