c74e616532ce023c50cb6e5b2480de652e2495b7
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-06-05 09:56:03 +0530

    Control Flow or IF Else

---
+++ control_flow.js
@@ -0,0 +1,40 @@
+/// if else statement
+//Conditional statements are used to perform different actions based on different conditions.
+
+// if(condition) {
+//    /// Body execute
+// } else{
+//     // Body execute
+// }
+
+
+// if (false) {
+//     console.log("Condition Is True");
+    
+// }else {
+//     console.log("Condition is False");
+    
+// }
+
+
+// let num = "Data";
+
+// if (num == "Data") {
+//     console.log("if body is True");
+    
+// } else {
+//     console.log("Else body true");
+    
+// }
+
+// && and,  || OR,    != not
+
+// ternary operator
+
+// condition ? true : false
+// let price = 100;
+
+// price >= 200 ? console.log("price is greater than 80") : console.log("Price is less");
+
+
+
