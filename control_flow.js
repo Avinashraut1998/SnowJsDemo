151e71e3be629c80ef382ff59ad0f56287873ea6
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-06-10 21:56:47 +0530

    Control Flow....

--- control_flow.js
+++ control_flow.js
@@ -37,4 +37,79 @@
 // price >= 200 ? console.log("price is greater than 80") : console.log("Price is less");
 
 
+// Switch case 
+
+let month = 3;
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default Case");
+        
+        break;
+}
+
+// let car = "a6"
+
+// switch (car) {
+//     case "Hummer":
+//           console.log("Hummer");
+          
+//         break;
+//     case "a6":
+//           console.log("a6");
+          
+//         break;
+//     case "BMW":
+//         console.log("BMW");
+        
+//         break;
+//     case "Vellar":
+//         console.log("Vellar");
+        
+//         break;
+
+//     default:
+//         console.log("No Car Matched");
+        
+//         break;
+// }
+
+
+
+
+
+// let Virat = "Kohli";
+
+//  switch (Virat) {
+//     case "Pandya":
+//         console.log("Virat Pandya")
+//         break;
+//     case "Sharma":
+//         console.log("Virat Sharma")
+//         break;
+//     case "Dhoni":
+//         console.log("Virat Dhoni")
+//         break;
+//     case "Kohl":
+//         console.log("Virat Kohli");
+//           break;      
+//     default:
+//         console.log("Not Matched ......!");
+        
+//         break;
+//  }
+
 
